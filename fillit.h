e1727abb9453d661794247187958501ad1ad01c0
Wesley Johanson <wjohanso@e1z3r7p11.42.us.org>
2020-02-06 11:31:18 -0800

    added the definitions of the blocks in the header file

--- fillit.h
+++ fillit.h
@@ -6,12 +6,28 @@
 /*   By: wjohanso <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/04 15:46:10 by wjohanso          #+#    #+#             */
-/*   Updated: 2020/02/04 16:05:23 by wjohanso         ###   ########.fr       */
+/*   Updated: 2020/02/06 11:30:56 by wjohanso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FILLIT_H
 # define FILLIT_H
-# define L_BLOCK {}
+# define I_BLOCK (int[8]) {0, 0, 0, 1, 0, 2, 0, 3}
+# define IR_BLOCK (int[8]) {0, 0, 1, 0, 2, 0, 3, 0}
+# define O_BLOCK (int[8]) {0, 0, 1, 0, 0, 1, 1, 1}
+# define T_BLOCK (int[8]) {1, 0, 0, 1, 1, 1, 2, 1}
+# define TR_BLOCK (int[8]) {0, 0, 0, 1, 1, 1, 0, 2}
+# define TD_BLOCK (int[8]) {0, 0, 1, 0, 2, 0, 1, 1}
+# define TL_BLOCK (int[8]) {1, 0, 0, 1, 1, 1, 1, 2}
+# define L_BLOCK (int[8]) {0, 0, 0, 1, 0, 2, 1, 2}
+# define LR_BLOCK (int[8]) {0, 0, 1, 0, 2, 0, 0, 1}
+# define LD_BLOCK (int[8]) {0, 0, 1, 0, 1, 1, 1, 2}
+# define LL_BLOCK (int[8]) {2, 0, 0, 1, 1, 1, 2, 1}
+# define J_BLOCK (int[8]) {1, 0, 1, 1, 1, 2, 0, 2}
+# define JR_BLOCK (int[8]) {0, 0, 0, 1, 1, 1, 2, 1}
+# define JD_BLOCK (int[8]) {0, 0, 1, 0, 0, 1, 0, 2}
+# define JL_BLOCK (int[8]) {0, 0, 1, 0, 0, 1, 0, 2}
+# define Z_BLOCK (int[8]) {0, 0, 1, 0, 1, 1, 2, 1}
+# define ZR_BLOCK (int[8]) {1, 0, 0, 1, 1, 1, 0, 2}
 
 #endif
