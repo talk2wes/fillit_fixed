100955d6cda30f6d4504616f859a51799745c255
Wesley Johanson <wjohanso@e1z3r7p11.42.us.org>
2020-02-07 14:03:23 -0800

    fillit.h was replaced back into the repo

---
+++ fillit.h
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fillit.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wjohanso <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/04 15:46:10 by wjohanso          #+#    #+#             */
+<<<<<<< HEAD
+/*   Updated: 2020/02/07 12:09:36 by wjohanso         ###   ########.fr       */
+=======
+/*   Updated: 2020/02/07 13:53:00 by wjohanso         ###   ########.fr       */
+>>>>>>> g_makefile
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FILLIT_H
+# define FILLIT_H
+
+# include <unistd.h>
+# include <fcntl.h>
+# include "./libft/libft.h"
+# define I_BLOCK (int[8]) {0, 0, 0, 1, 0, 2, 0, 3}
+# define IR_BLOCK (int[8]) {0, 0, 1, 0, 2, 0, 3, 0}
+# define O_BLOCK (int[8]) {0, 0, 1, 0, 0, 1, 1, 1}
+# define T_BLOCK (int[8]) {1, 0, 0, 1, 1, 1, 2, 1}
+# define TR_BLOCK (int[8]) {0, 0, 0, 1, 1, 1, 0, 2}
+# define TD_BLOCK (int[8]) {0, 0, 1, 0, 2, 0, 1, 1}
+# define TL_BLOCK (int[8]) {1, 0, 0, 1, 1, 1, 1, 2}
+# define L_BLOCK (int[8]) {0, 0, 0, 1, 0, 2, 1, 2}
+# define LR_BLOCK (int[8]) {0, 0, 1, 0, 2, 0, 0, 1}
+# define LD_BLOCK (int[8]) {0, 0, 1, 0, 1, 1, 1, 2}
+# define LL_BLOCK (int[8]) {2, 0, 0, 1, 1, 1, 2, 1}
+# define J_BLOCK (int[8]) {1, 0, 1, 1, 1, 2, 0, 2}
+# define JR_BLOCK (int[8]) {0, 0, 0, 1, 1, 1, 2, 1}
+# define JD_BLOCK (int[8]) {0, 0, 1, 0, 0, 1, 0, 2}
+# define JL_BLOCK (int[8]) {0, 0, 1, 0, 0, 1, 0, 2}
+# define Z_BLOCK (int[8]) {0, 0, 1, 0, 1, 1, 2, 1}
+# define ZR_BLOCK (int[8]) {1, 0, 0, 1, 1, 1, 0, 2}
+
+typedef struct	s_blocks
+{
+	void			*block_id;
+	char			c;
+	struct s_blocks *next;
+}				t_blocks;
+
+typedef struct	s_text
+{
+	char			*str;
+	struct s_text	*next;
+}				t_text;
+
+#endif
