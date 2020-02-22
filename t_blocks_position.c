3ef8ce09a76f491725699315692ce6f75474a97c
Wesley Johanson <wjohanso@e1z2r5p33.42.us.org>
2020-02-21 19:20:47 -0800

    small bug fix

--- t_blocks_position.c
+++ t_blocks_position.c
@@ -6,7 +6,7 @@
 /*   By: wjohanso <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/21 11:19:28 by wjohanso          #+#    #+#             */
-/*   Updated: 2020/02/21 12:48:51 by wjohanso         ###   ########.fr       */
+/*   Updated: 2020/02/21 19:19:17 by wjohanso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,12 @@
 
 void	t_blocks_change_delta(t_blocks *blocks, int i, int j)
 {
-	int		i;
+	int		x;
 
-	i = 0;
+	x = 0;
 	(*blocks).delta[0] = i;
 	(*blocks).delta[1] = j;
-	while (i < GRID_LENGTH)
+	while (x < GRID_LENGTH)
 	{
 		(*blocks).x_position[i] = (*blocks).x_loc[i] + (*blocks).delta[0];
 		(*blocks).y_position[i] = (*blocks).y_loc[i] + (*blocks).delta[1];
