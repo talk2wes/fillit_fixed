8b05ee89f9619c5dbdfb264544a0038746bc6ada
jane <janeodum41@gmail.com>
2020-02-25 15:10:41 -0800

    remeber to push

--- map.c
+++ map.c
@@ -86,6 +86,27 @@ void free_map(char **map, int map_size)
 	}
 	free(map);
 }
+
+// void	free_list(t_inputmap *head)
+// {
+// 	t_blocks	*tmp;
+// 	t_inputmap	input;
+// 	int i;
+
+// 	i = 0;
+// 	while (head != NULL)
+// 	{
+// 		tmp = head;
+// 		i = 0;
+// 		while (i < 4)
+// 		{
+// 			free((*tmp).str[i]);
+// 			i++;
+// 		}
+// 		head = head->next;
+// 		free(tmp);
+// 	}
+// }
 void	print_board(char **map, int map_size)
 {
 	int i;
