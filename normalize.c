ff7d0897255765e277fd0cbba67917fd9cba49fe
jane <janeodum41@gmail.com>
2020-02-25 15:09:51 -0800

    trying to fix this shit

--- normalize.c
+++ normalize.c
@@ -17,33 +17,33 @@
  */
 
 
-void smallest_xy_tetris(t_blocks *piece, int x, int y)
-{
-	int i;
+// void smallest_xy_tetris(t_blocks *piece, int x, int y)
+// {
+// 	int i;
 	
-	int smallest_pos_x = (*piece).x_loc[0];
-	int smallest_pos_y = (*piece).y_loc[0];
-	i = 1;
-	while (i < 4)
-	{
-		if ((*piece).x_loc[i] < smallest_pos_x)
-		{
-			smallest_pos_x = (*piece).x_loc[i];
-		}
-		if ((*piece).y_loc[i] < smallest_pos_y)
-		{
-			smallest_pos_y = (*piece).y_loc[i];
-		}
-		i++;
-	}
-	i--;
-	while (i >= 0)
-	{
-		(*piece).x_loc[i] = (*piece).x_loc[i] - smallest_pos_x + x;
-		(*piece).y_loc[i] = (*piece).y_loc[i] - smallest_pos_y + y;
-		i--;
-	}
-}
+// 	int smallest_pos_x = (*piece).x_loc[0];
+// 	int smallest_pos_y = (*piece).y_loc[0];
+// 	i = 1;
+// 	while (i < 4)
+// 	{
+// 		if ((*piece).x_loc[i] < smallest_pos_x)
+// 		{
+// 			smallest_pos_x = (*piece).x_loc[i];
+// 		}
+// 		if ((*piece).y_loc[i] < smallest_pos_y)
+// 		{
+// 			smallest_pos_y = (*piece).y_loc[i];
+// 		}
+// 		i++;
+// 	}
+// 	i--;
+// 	while (i >= 0)
+// 	{
+// 		(*piece).x_loc[i] = (*piece).x_loc[i] - smallest_pos_x + x;
+// 		(*piece).y_loc[i] = (*piece).y_loc[i] - smallest_pos_y + y;
+// 		i--;
+// 	}
+// }
 
 
 void	align_blocks(t_blocks *blocks)
