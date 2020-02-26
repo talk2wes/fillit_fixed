0b815f20ffc99b4a9f88fd7b255d970c62a6170e
jane <janeodum41@gmail.com>
2020-02-25 17:40:28 -0800

    We are finally getting there

--- valid_tetris.c
+++ valid_tetris.c
@@ -6,7 +6,7 @@
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/03 11:33:03 by wjohanso          #+#    #+#             */
 /*   Updated: 2020/02/13 13:42:51 by wjohanso         ###   ########.fr       */
-/*   Updated: 2020/02/20 22:18:13 by wjohanso         ###   ########.fr       */
+/*   Updated: 2020/02/25 17:39:57 by jaodum           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,7 +58,8 @@ int			valid_tetris(int fd, t_blocks *blocks)
 			}
 			l_len++;
 		}
-		if ((l_len != 0 || l_len != 4) && num_lines % 5 == 0 && **line != '\0')
+		//printf("l_len = %i\n",l_len);
+		if ((l_len != 0 || l_len != 4) || ( num_lines % 5 == 0 && **line != '\0'))
 		{
 			//printf("VALID_TETRIS: Invalid line len OR Tetriminos LEN\n");
 			//printf("l_len = %i\t num_lines = %i\t **line = '%c'\n",
