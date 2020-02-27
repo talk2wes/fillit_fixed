56b31de8b01d67777bb77817c7d84e8231231d52
Wesley Johanson <wjohanso@e1z2r5p33.42.us.org>
2020-02-27 00:20:56 -0800

    fillit.h norm errors fixed

--- fillit.h
+++ fillit.h
@@ -5,17 +5,14 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: wjohanso <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/02/04 15:46:10 by wjohanso          #+#    #+#             */
-/*   Updated: 2020/02/21 12:40:15 by wjohanso         ###   ########.fr       */
-/*   Updated: 2020/02/07 13:53:00 by wjohanso         ###   ########.fr       */
+/*   Created: 2020/02/27 00:17:17 by wjohanso          #+#    #+#             */
+/*   Updated: 2020/02/27 00:20:36 by wjohanso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-//update header file 2/20 300pm 
+
 #ifndef FILLIT_H
 # define FILLIT_H
 # include "libft/libft.h"
-# include <stdio.h> // REMOVE THIS FOR DEBUGGING 
-# include <string.h> // REMOVE THIS FOR DEBUGGING 
 # include <stdlib.h>
 # include <unistd.h>
 # include <fcntl.h>
@@ -33,11 +30,11 @@
 # define INITIAL_DELTA -1
 # define INITIAL_POSITION -1
 
-typedef	struct		s_coords
+typedef	struct	s_coords
 {
 	int				row;
 	int				col;
-}					t_coords;
+}				t_coords;
 
 typedef struct	s_blocks
 {
@@ -52,33 +49,30 @@ typedef struct	s_blocks
 
 typedef struct	s_inputmap
 {
-	char			str[END_INDEX + 2];
+	char		str[END_INDEX + 2];
 }				t_inputmap;
 
-int			valid_tetris(int fd, t_blocks *blocks);
-void		input_map_store(char **line, int line_num, t_inputmap
-			*input);
-int			adjacency_counter(char *str);
-void		t_blocks_init(t_blocks *blocks);
-void		t_blocks_store(t_inputmap input, t_blocks *blocks);
-void		inputmap_reset(t_inputmap *input);
-t_blocks	*t_blocks_newnode();
-void		t_blocks_print_data(t_blocks *blocks);
-void		align(t_blocks *blocks);
-void		align_blocks(t_blocks *blocks);
-void		smallest_xy_tetris(t_blocks *piece, int x, int y);
-void		t_blocks_change_delta(t_blocks *blocks, int i, int y);
-//void		t_blocks_update_position(t_blocks *blocks);
-int			check_Overlap(char **map, t_blocks *piece, int map_size);
-int tetris_count(t_blocks *head);
-int		board_size(int tetris_count);
-int		find_sqrt(int n);
-char	**create_empty_board(int map_size);
-void free_map(char **map, int map_size);
-char	**increase_map_size(char **map, int map_size);
-char **remove_tetris(char **map, t_blocks *piece, int map_size);
-void place(char **map, t_blocks *piece, char letter);
-char		**backtrack(char **tetris_map, t_blocks *piece, int map_size);
-void		solve(t_blocks *piece, char **map, int map_size);
-void		print_board(char **map, int b_size);
+int				valid_tetris(int fd, t_blocks *blocks);
+void			input_map_store(char **line, int line_num, t_inputmap *input);
+int				adjacency_counter(char *str);
+void			t_blocks_init(t_blocks *blocks);
+void			t_blocks_store(t_inputmap input, t_blocks *blocks);
+void			inputmap_reset(t_inputmap *input);
+t_blocks		*t_blocks_newnode();
+void			t_blocks_print_data(t_blocks *blocks);
+void			align(t_blocks *blocks);
+void			align_blocks(t_blocks *blocks);
+void			smallest_xy_tetris(t_blocks *piece, int x, int y);
+void			t_blocks_change_delta(t_blocks *blocks, int i, int y);
+int				tetris_count(t_blocks *head);
+int				board_size(int tetris_count);
+int				find_sqrt(int n);
+char			**create_empty_board(int map_size);
+void			free_map(char **map, int map_size);
+char			**increase_map_size(char **map, int map_size);
+char			**remove_tetris(char **map, t_blocks *piece, int map_size);
+void			place(char **map, t_blocks *piece, char letter);
+char			**backtrack(char **tetris_map, t_blocks *piece, int map_size);
+void			solve(t_blocks *piece, char **map, int map_size);
+void			print_board(char **map, int b_size);
 #endif
