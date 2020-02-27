0503bf8946f284cbc4d32fe4760d4dde41faa88b
Wesley Johanson <wjohanso@e1z2r5p33.42.us.org>
2020-02-26 23:22:16 -0800

    fillit.c norm errors fixed

--- fillit.c
+++ fillit.c
@@ -6,7 +6,7 @@
 /*   By: wjohanso <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/04 15:38:17 by wjohanso          #+#    #+#             */
-/*   Updated: 2020/02/26 23:14:37 by wjohanso         ###   ########.fr       */
+/*   Updated: 2020/02/26 23:21:56 by wjohanso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,24 @@
 
 int		main(int argc, char **argv)
 {
-	int		fd;
-	int		out;
+	int			fd;
+	int			out;
 	t_blocks	*blocks;
 
-	blocks = t_blocks_newnode();
-	t_blocks_init(blocks);
-	out = -1;
+	t_blocks_init((blocks = t_blocks_newnode()));
 	fd = 0;
-	if (argc == 2 && (fd = open(argv[1], O_RDONLY)) >= 0 && (out = valid_tetris(fd, blocks) == 1))
+	if (argc == 2 && (fd = open(argv[1], O_RDONLY)) >= 0 &&
+			(out = valid_tetris(fd, blocks) == 1))
 	{
-		//out = valid_tetris(fd, blocks);
 		align_blocks(blocks);
 		if (out == 1)
 			solve(blocks, create_empty_board(board_size(tetris_count(blocks))),
-				  board_size(tetris_count(blocks)));
+				board_size(tetris_count(blocks)));
 		else
 			write(1, "error\n", 6);
 	}
 	else
 		write(1, "error\n", 6);
-	close(fd);	
+	close(fd);
 	return (0);
 }
