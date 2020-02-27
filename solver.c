e2d9cee42af965d0495885021c0e9aca7b883191
Wesley Johanson <wjohanso@e1z2r5p33.42.us.org>
2020-02-27 00:12:38 -0800

    solver.c norm errors besides one comment (which may help with preventing memory leaks) have been solved

--- solver.c
+++ solver.c
@@ -1,7 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   solver.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wjohanso <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/26 23:58:52 by wjohanso          #+#    #+#             */
+/*   Updated: 2020/02/27 00:11:59 by wjohanso         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "fillit.h"
 
-
-int		check_Overlap(char **map, t_blocks *piece, int map_size)
+int			check_overlap(char **map, t_blocks *piece, int map_size)
 {
 	int i;
 	int col;
@@ -14,7 +25,8 @@ int		check_Overlap(char **map, t_blocks *piece, int map_size)
 		col = 0;
 		while (col < map_size)
 		{
-			if ((*piece).x_position[i] < map_size && (*piece).y_position[i] < map_size
+			if ((*piece).x_position[i] < map_size &&
+				(*piece).y_position[i] < map_size
 				&& map[(*piece).x_position[i]][(*piece).y_position[i]] == '.')
 			{
 				i++;
@@ -25,16 +37,15 @@ int		check_Overlap(char **map, t_blocks *piece, int map_size)
 		}
 		row++;
 	}
-		return (0);
+	return (0);
 }
-char **remove_tetris(char **map, t_blocks *piece, int map_size)
+
+char		**remove_tetris(char **map, t_blocks *piece, int map_size)
 {
-	//printf("remove_tetris map size = %i\n", map_size);
-	int x;
-	int y;
+	int		x;
+	int		y;
+
 	x = 0;
-
-
 	while (x < map_size)
 	{
 		y = 0;
@@ -46,43 +57,34 @@ char **remove_tetris(char **map, t_blocks *piece, int map_size)
 		}
 		x++;
 	}
-	//prtinf("mapsize RETURN\n");
 	return (map);
 }
 
-void place(char **map, t_blocks *piece, char letter)
+void		place(char **map, t_blocks *piece, char letter)
 {
-	int x;
-	int y;
-	int i;
+	int		x;
+	int		y;
+	int		i;
+
 	i = 0;
-	//printf("place function\n");
 	while (i < 4)
 	{
 		x = piece->x_position[i];
 		y = piece->y_position[i];
 		map[x][y] = letter;
-		// printf("%d %d", x, y);
-		// printf("printing inside the place function");
-		// print_board(map, map_size);
-		// printf("\n");
 		i++;
 	}
-	//return (map);
 }
 
-
-char **backtrack(char **tetris_map, t_blocks *piece, int map_size)
+char		**backtrack(char **tetris_map, t_blocks *piece, int map_size)
 {
-	int row;
-	int col;
+	int		row;
+	int		col;
+	char	**map;
+
 	row = -1;
-	char **map;
-	
 	if (piece == NULL)
-	{
-		return tetris_map;
-	}
+		return (tetris_map);
 	map = NULL;
 	while (++row < map_size)
 	{
@@ -90,35 +92,23 @@ char **backtrack(char **tetris_map, t_blocks *piece, int map_size)
 		while (col < map_size)
 		{
 			t_blocks_change_delta(piece, col++, row);
-			if (check_Overlap(tetris_map, piece, map_size))
+			if (check_overlap(tetris_map, piece, map_size))
 			{
-				//printf("backtrack letter %c\n",(*piece).letter);
-				//print_board(tetris_map, map_size);
-				// printf("\n");
-				place(tetris_map, piece,piece->letter);
+				place(tetris_map, piece, piece->letter);
 				map = (backtrack(tetris_map, piece->next, map_size));
-				
-				
 			}
 			if (map)
-			{
 				return (map);
-			}
 			tetris_map = remove_tetris(tetris_map, piece, map_size);
 		}
 	}
-	return NULL;	
-
+	return (NULL);
 }
 
-void	solve(t_blocks *piece, char **map, int map_size)
+void		solve(t_blocks *piece, char **map, int map_size)
 {
 	while (!backtrack(map, piece, map_size))
-	{
-
-		map = increase_map_size(map, map_size);
-		map_size++;
-	}
+		map = increase_map_size(map, map_size++);
 	print_board(map, map_size);
 	free_map(map, map_size);
 	//free_list(*piece);
