091563977b3707a8b56b4d650b866dc935ddf4eb
Wesley Johanson <wjohanso@e1z2r5p33.42.us.org>
2020-02-26 23:56:07 -0800

    input_map_store.c norm errors fixed

--- input_map_store.c
+++ input_map_store.c
@@ -6,30 +6,27 @@
 /*   By: wjohanso <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/17 11:15:12 by wjohanso          #+#    #+#             */
-/*   Updated: 2020/02/20 21:48:19 by wjohanso         ###   ########.fr       */
+/*   Updated: 2020/02/26 23:55:49 by wjohanso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
-void		input_map_store(char **line, int line_num, t_inputmap *input)
+void	input_map_store(char **line, int line_num, t_inputmap *input)
 {
 	int		i;
+
 	i = 0;
 	if (line_num % 5 == 0)
 	{
-		//printf("RESET INPUT.STR\n");
 		inputmap_reset(input);
 		return ;
 	}
 	if (line_num > 5)
 		line_num = line_num % 5;
 	while (i < 4 && line_num % 5 != 0 && line_num < MAXIMUM_LINES)
-	{	
-		//printf("index = %i\tline_num = %i",(line_num - 1)*4 + i,line_num);
-		//printf("\tchar = %c\n",line[0][i]);
-		(*input).str[(line_num - 1)*4 + i] = line[0][i];
+	{
+		(*input).str[(line_num - 1) * 4 + i] = line[0][i];
 		i++;
 	}
-	//printf("line = \t\t%s\ninput = \t%s\n\n",*line,(*input).str);
 }
