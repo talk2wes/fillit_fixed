5d04029b08c4e29dc2dbb312febb2c4b14a89e9b
Wesley Johanson <wjohanso@e1z2r7p5.42.us.org>
2020-02-28 12:55:59 -0800

    valid_tetris.c norm errors are fixed! now let's hope that there isn't too many memory leaks

--- valid_tetris.c
+++ valid_tetris.c
@@ -6,7 +6,7 @@
 /*   By: wjohanso <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/27 00:38:39 by wjohanso          #+#    #+#             */
-/*   Updated: 2020/02/28 12:36:03 by wjohanso         ###   ########.fr       */
+/*   Updated: 2020/02/28 12:55:15 by wjohanso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,28 +30,7 @@ static int	adjacency_n_store(int num_lines, t_inputmap input,
 		return (0);
 	return (1);
 }
-/*
-static void		helper(char **line, )
-{
-	int		hashes;
 
-	
-	hashes = (num_lines % 5 == 0) ? 0 : hashes;
-		num_lines++;
-		l_len = 0;
-		while (line[0][l_len] != '\n' && line[0][l_len] != 0)
-		{
-			hashes = (line[0][l_len] == '#') ? hashes + 1 : hashes;
-			if (line[0][l_len] != '.' && line[0][l_len] != '#')
-				return (0);
-			l_len++;
-		}
-		if (!check_input_a(l_len, num_lines, hashes, line))
-			return (0);
-		input_map_store(line, num_lines, &input);
-		if (!adjacency_n_store(num_lines, input, blocks))
-			return (0);
-*/
 void		helper(int *num_lines, int *hashes, int *l_len)
 {
 	*hashes = (*num_lines % 5 == 0) ? 0 : *hashes;
@@ -73,25 +52,24 @@ int			valid_tetris(int fd, t_blocks *blocks)
 	int			hashes;
 	int			num_lines;
 	char		**line;
-	int			ret;
-	int			l_len;
 	t_inputmap	input;
+	int			ret_len[2];
 
 	num_lines = 0;
 	line = (char**)ft_strnew(0);
-	while ((ret = get_next_line(fd, line)) == 1)
+	while ((ret_len[0] = get_next_line(fd, line)) == 1)
 	{
-		helper(&num_lines, &hashes, &l_len);
-		while (line[0][l_len] != '\n' && line[0][l_len] != 0)
-			if (!helper2(line, &l_len, &hashes))
+		helper(&num_lines, &hashes, &ret_len[1]);
+		while (line[0][ret_len[1]] != '\n' && line[0][ret_len[1]] != 0)
+			if (!helper2(line, &ret_len[1], &hashes))
 				return (0);
-		if (!check_input_a(l_len, num_lines, hashes, line))
+		if (!check_input_a(ret_len[1], num_lines, hashes, line))
 			return (0);
 		input_map_store(line, num_lines, &input);
 		if (!adjacency_n_store(num_lines, input, blocks))
 			return (0);
 	}
 	ft_strdel(line);
-	return (num_lines > MAXIMUM_LINES || num_lines % 5 != 4 || ret == -1) ?
-	0 : 1;
+	return (num_lines > MAXIMUM_LINES || num_lines % 5 != 4 || ret_len[0] == -1)
+	? 0 : 1;
 }
